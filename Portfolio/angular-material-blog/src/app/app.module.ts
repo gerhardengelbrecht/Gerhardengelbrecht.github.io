8c6ad0497725dcaf1fcbd96152f3262ded6ddb11
gerhard.vapsure <gerhard.vapsure@PC66>
2018-08-27 13:26:48 +0200

    moved angular-material-blog from sandbux

---
+++ Portfolio/angular-material-blog/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DataService } from './data/data.service';
+import { AuthService } from './auth.service';
+import { PostDialogComponent } from './post-dialog/post-dialog.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent,
+    PostDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    DataService,
+    AuthService
+  ],
+  entryComponents: [
+    PostDialogComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
